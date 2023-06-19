a362c9fd3d5e9345c128ddfb81e7bc265861bc40
Edson Junior <edson.junior@mereo.com>
2023-06-19 09:48:52 -0300

    testa método sem retorno(void)

---
+++ Learning_Unit_tests/ErrorLoggerTests.cs
@@ -0,0 +1,19 @@
+﻿
+using TestesUnitários.Fundamentos;
+
+namespace Learning_Unit_tests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty() 
+        {
+            var logger = new ErrorLogger();
+
+            logger.Log("a");
+
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+    }
+}
