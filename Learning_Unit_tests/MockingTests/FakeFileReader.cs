0b4f5e852ed9a761c2c73d32e2ffecf112fd2991
Edson Junior <edson.junior@mereo.com>
2023-06-24 10:16:41 -0300

    Iniciando os tests utilizando MockingTests

---
+++ Learning_Unit_tests/MockingTests/FakeFileReader.cs
@@ -0,0 +1,12 @@
+﻿using TestesUnitários.Mocking;
+
+namespace Learning_Unit_tests.MockingTests
+{
+    public class FakeFileReader : IFileReader
+    {
+        public string Read(string path)
+        {
+            return "";
+        }
+    }
+}
