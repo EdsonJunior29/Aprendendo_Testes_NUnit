bba9b7c8cb75aa8a3a2d924a5ed323e8c0910c11
Edson Junior <edson.junior@mereo.com>
2023-06-24 11:32:09 -0300

    Injeção de dependência via propriedade

--- Learning_Unit_tests/Mocking/VideoServiceTests.cs
+++ Learning_Unit_tests/Mocking/VideoServiceTests.cs
@@ -10,8 +10,10 @@ namespace Learning_Unit_tests.Mocking
         public void ReadVideoTitle_EmptyFile_ReturnError()
         { 
             var service = new VideoService();
+            // FileReader = propriedade da class VideoService
+            service.FileReader = new FakeFileReader();
 
-            var result = service.ReadVideoTitle(new FakeFileReader());
+            var result = service.ReadVideoTitle();
 
             Assert.That(result, Does.Contain("error").IgnoreCase);
         }
