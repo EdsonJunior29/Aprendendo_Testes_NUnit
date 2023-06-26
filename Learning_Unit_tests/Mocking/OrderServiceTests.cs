c297c5fd2c4da43c2f0473818d0adb679899db29
Edson Junior <edson.junior@mereo.com>
2023-06-26 10:12:43 -0300

    adicionando a OrderServiceTests

---
+++ Learning_Unit_tests/Mocking/OrderServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using Moq;
+using TestesUnitários.Mocking;
+
+namespace Learning_Unit_tests.Mocking
+{
+    [TestFixture]
+    public class OrderServiceTests
+    {
+        [Test]
+        public void PlaceOrder_WhenCalled_StoreTheOrder() 
+        {
+            var storage = new Mock<IStorage>();
+            var service = new OrderService(storage.Object);
+            var order = new Order();
+
+            service.PlaceOrder(order);
+
+            storage.Verify(s => s.Store(order));
+        }
+    }
+}
