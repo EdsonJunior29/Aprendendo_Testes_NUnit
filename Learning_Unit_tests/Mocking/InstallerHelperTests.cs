6e85df849486d550b3f4838929e38ad23d601b5e
Edson Junior <edson.junior@mereo.com>
2023-07-01 08:12:33 -0300

    Testes de requisições para URLs

---
+++ Learning_Unit_tests/Mocking/InstallerHelperTests.cs
@@ -0,0 +1,44 @@
+﻿
+using Moq;
+using System.Net;
+using TestesUnitários.Mocking;
+
+namespace Learning_Unit_tests.Mocking
+{
+    [TestFixture]
+    public class InstallerHelperTests
+    {
+        private Mock<IFileDownloader> _fileDownloader;
+        private InstallerHelper _installerHelper;
+
+        [SetUp]
+        public void SetUp()
+        { 
+            _fileDownloader = new Mock<IFileDownloader>();
+            _installerHelper = new InstallerHelper(_fileDownloader.Object);
+        }
+
+        [Test]
+        public void DownloadInstaller_WhereDownloadFail_ReturnFalse()
+        {
+            // criando o Mock
+            // It.IsAny<string>() = Forma genérica de passar parâmetros
+            _fileDownloader.Setup(fd => fd.DownloadFile(It.IsAny<string>(), It.IsAny<string>())).Throws<WebException>();
+
+            var result = _installerHelper.DownloadInstaller("customer", "installer");
+
+            Assert.IsFalse(result);
+
+        }
+
+        [Test]
+        public void DownloadInstaller_WhereDownloadCompletes_ReturnTrue()
+        {
+           
+            var result = _installerHelper.DownloadInstaller("customer", "installer");
+
+            Assert.IsTrue(result);
+
+        }
+    }
+}
